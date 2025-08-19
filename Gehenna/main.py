bbafddce4a2253f32099b3f04a7085ec78117854
BBQ-Is-Taken <jwlavender2@gmail.com>
2025-08-19 18:14:21 -0400

    Update main.py
    Improved Main

--- Gehenna/main.py
+++ Gehenna/main.py
@@ -1,7 +1,10 @@
-from randomCharacter import makeCharacterList, printCharacters
-from randomLocation import makeLocationList, printLocations
+from randomCharacter import makeCharacterList
+from randomItem import makeItemList, inABCOrder
+from randomLocation import makeLocationList
+from misc import printList, randomIntensity
 
 quit = True
+numOf = 0
 
 while quit:
     select = input("Make a selection! (h for help):  ")
@@ -9,11 +12,37 @@ while quit:
     if select == "q":
         break
     elif select == "h":
-        print("Choose Locations or Characters")
-    elif select == "Locations":
-        printLocations()
-    elif select == "Characters":
-        printCharacters()
+        print("Choose: Locations, Characters, Items, Intensity, or All")
+        
+    elif select == "Locations" or select == "L":
+        numOf = input("How many Locations: ")
+        printList(makeLocationList(numOf))
+        
+    elif select == "Characters" or select == "C":
+        numOf = input("How many Characters: ")
+        printList(makeCharacterList(numOf))
+        
+    elif select == "Items" or select == "I":
+        numOf = input("How many Items: ")
+        printList(makeItemList(numOf))
+    
+    elif select == "Intensity":
+        print(randomIntensity())
+        
+    elif select == "All":
+        numOf2, numOf3 = 0, 0
+        numOf = input("How many Locations: ")
+        numOf2 = input("How many Characters: ")
+        numOf3 = input("How many Items: ")
+        
+        printList(makeLocationList(numOf))
+        printList(makeCharacterList(numOf2))
+        printList(makeItemList(numOf3))
+        print(randomIntensity())
+        
+    elif select == "ABC":
+        inABCOrder()
+        
     else:
         print("Choose an option!")
     print()
